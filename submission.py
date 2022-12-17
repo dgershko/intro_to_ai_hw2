2d406a9ef2dcdc95d18cafa695d2ac1d5990e581
dgershko <dgershko@gmail.com>
2022-12-17 22:47:56 +0200

    rb_minimax working, needs optimizations

--- submission.py
+++ submission.py
@@ -1,5 +1,8 @@
 import random
 import math
+import time
+import multiprocessing
+import functools
 
 import numpy as np
 
@@ -68,9 +71,7 @@ def dumb_heuristic2(state, agent_id):
     return sum_pawns
 
 
-
-
-def get_unhidden_trios(state: gge.State, agent_id):
+def get_positional_value(state: gge.State, agent_id):
     player_pawns = [state.player1_pawns, state.player2_pawns]
     board = np.zeros([3,3], dtype=int)
     for key, value in player_pawns[agent_id].items():
@@ -78,26 +79,26 @@ def get_unhidden_trios(state: gge.State, agent_id):
             continue
         (x, y) = value[0]
         board[x, y] = Superparams().size_to_value[value[1]].value
-    trios = [np.zeros(3, dtype=int), np.zeros(3, dtype=int)]
+    trios = [board.diagonal(), np.fliplr(board).diagonal()]
     for i in range(3):
-        trios.append(board[i,:])
-        trios.append(board[:,i])
-        trios[0][i] = board[i,i]
-        trios[1][i] = board[i,2-i]
-    return trios
+        trios.extend([board[i,:],board[:,i]])
+    
+    value = 0
+    for trio in trios:
+        value += (np.count_nonzero(trio) ** Superparams().combo_weight) * np.sum(trio)
+    return value
 
-
-def intermediate_heuristic(state: gge.State, agent_id):
-    # Parameters and consts
-    # power_superparam = 1  # By increasing this we're making sure the agent favors combos (eg. 2 in a row)
-    # eaten_weight_superparam = 1  # controls the weighting of eating vs getting combos
+@functools.lru_cache(maxsize=256)
+def smart_heuristic(state: gge.State, agent_id):
+    # # Parameters and consts
+    # # power_superparam = 1  # By increasing this we're making sure the agent favors combos (eg. 2 in a row)
+    # # eaten_weight_superparam = 1  # controls the weighting of eating vs getting combos
     heuristic_value = 0
     position_value = 0
     eaten_value = 0
-    
-    # Sum trios values (by the formula stated in the dry section)
-    for trio in get_unhidden_trios(state, agent_id):
-        position_value += (np.count_nonzero(trio) ** Superparams().combo_weight) * np.sum(trio)
+
+    #TODO: make these three calculations parrallel?
+    position_value = get_positional_value(state, agent_id) - get_positional_value(state, 1 - agent_id)
     
     # Sum values of enemy eaten (hidden) goblins
     player_pawns = [state.player1_pawns, state.player2_pawns]
@@ -107,11 +108,6 @@ def intermediate_heuristic(state: gge.State, agent_id):
     
     heuristic_value = position_value + Superparams().eating_weight * eaten_value
     return heuristic_value
-    
-
-def smart_heuristic(state: gge.State, agent_id):
-    return intermediate_heuristic(state, agent_id) - Superparams().enemy_heuristic_weight * intermediate_heuristic(state, 1 - agent_id)
-
 
 
 # IMPLEMENTED FOR YOU - NO NEED TO CHANGE
@@ -160,9 +156,85 @@ def greedy_improved(curr_state, agent_id, time_limit):
     return max_neighbor[0]
 
 
-def rb_heuristic_min_max(curr_state, agent_id, time_limit):
-    raise NotImplementedError()
+#==========================================#
+# ---------------RB MINIMAX--------------- #
+#==========================================#
 
+class Minimaxer():
+    def __init__(self, caller_agent):
+        self.agent_id = caller_agent
+        self.current_agent = caller_agent
+
+    # Get the state's value as viewed by a certain agent (min/max)
+    # The current agent doesn't affect final states
+    # The current agent does affect heuristic values
+    @functools.cache
+    def state_value(self, state: gge.State, depth):
+        match_state = gge.is_final_state(state)
+        if match_state is not None:
+            match_state = int(match_state) - 1
+            if match_state == self.agent_id:
+                return math.inf
+            elif match_state == 1 - self.agent_id:
+                return -math.inf
+            return 0
+        
+        if depth == 0:
+            return smart_heuristic(state, self.agent_id)
+
+        if self.current_agent == self.agent_id:
+            return self.min_value(state, depth)
+        else:
+            return self.max_value(state, depth)
+
+    @functools.lru_cache(maxsize=128)
+    def max_value(self, state: gge.State, depth):
+        self.current_agent = 1 - self.current_agent
+        new_depth = depth - 1
+        value = max([self.state_value(neighbor[1], new_depth) for neighbor in state.get_neighbors()])
+        return value
+
+    @functools.lru_cache(maxsize=128)
+    def min_value(self, state: gge.State, depth):
+        self.current_agent = 1 - self.current_agent
+        new_depth = depth - 1
+        value = min([self.state_value(neighbor[1], new_depth) for neighbor in state.get_neighbors()]) 
+        return value
+
+
+def rb_heuristic_min_max(curr_state: gge.State, agent_id, time_limit):
+    def rb_iteration(neighbor_list, agent_id, depth, child_conn):
+        begin = time.time()
+        minimaxer = Minimaxer(agent_id)
+        best_action = None
+        max_neighbor_value = -math.inf
+        for neighbor in neighbor_list:
+            move_value = minimaxer.state_value(neighbor[1], depth)
+            if move_value > max_neighbor_value:
+                best_action = neighbor[0]
+                max_neighbor_value = move_value
+        print(f"depth: {depth}, elapsed time: {time.time() - begin}")
+        child_conn.send(best_action)
+
+    #TODO: missing default move in case of instant timeout?
+    start_time = time.time()
+    neighbor_list = curr_state.get_neighbors()
+    depth = 0
+    best_action = None
+    parent_conn, child_conn = multiprocessing.Pipe() # ayy lmao
+    while True:
+        minimax_iteration_process = multiprocessing.Process(target=rb_iteration, args=(neighbor_list, agent_id, depth, child_conn))
+        minimax_iteration_process.start()
+        remaining_time = time_limit - (time.time() - start_time)
+        minimax_iteration_process.join(remaining_time - 0.05)
+        if minimax_iteration_process.is_alive(): # if job is still alive after the remaining time ran out, its time to /die/
+            print("timeout")
+            minimax_iteration_process.kill()
+            print(f"best action: {best_action}")
+            return best_action
+        best_action = parent_conn.recv() # update best action after iteration is done
+        depth +=1 # prepare for next iteration with more depth
+    
 
 def alpha_beta(curr_state, agent_id, time_limit):
     raise NotImplementedError()
